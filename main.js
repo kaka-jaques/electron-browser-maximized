631c444cc212108990f86dc90bf601de4f2ffc51
Kalil J. Fakhouri <kalil@showtec.local>
2022-10-18 17:28:40 -0300

    INITIAL COMMIT

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow = null
+
+app.whenReady().then(() => {
+  // We cannot require the screen module until the app is ready.
+  const { screen } = require('electron')
+
+  // Create a window that fills the screen's available work area.
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const { width, height } = primaryDisplay.workAreaSize
+
+  mainWindow = new BrowserWindow({ width, height })
+  mainWindow.loadURL('http://localhost:4200')
+})
